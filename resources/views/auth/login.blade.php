f5c78dea9e6e6e60ad7d2b5ee36455b8e76aee99
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-02-25 16:40:43 +0000

    changement dans le UI du login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,26 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Sniglet&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
     <style>
         body {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            /* Police plus moderne */
+            font-family: 'Sniglet', cursive;
         }
     </style>
 </head>
 
-<body class="h-screen overflow-hidden flex bg-indigo-50 text-gray-800">
+<body class="h-screen overflow-hidden flex bg-purple-50 text-gray-800">
 
     <!-- Partie Image (50% de la largeur) -->
     <div class="w-1/2 h-screen">
-        <img src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        <img src="https://img.freepik.com/vecteurs-libre/illustration-notion-comptabilite_114360-16010.jpg?t=st=1740499472~exp=1740503072~hmac=74c9ceb6fe7a705225886ae0b9a18d8bf1721fa564a42a8efd854f6aa6cc2ef9&w=900"
             alt="Illustration de gestion financière" class="object-cover h-full w-full">
     </div>
 
     <!-- Partie Formulaire de Connexion (50% de la largeur) -->
-    <div class="w-1/2 flex items-center justify-center">
+    <div class="w-1/2 flex items-center justify-center bg-gradient-to-br from-purple-200 to-purple-400">
         <div class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 w-3/4">
-            <h1 class="block text-indigo-700 text-2xl font-bold mb-6 text-center">Connexion</h1>
+            <div class="text-center mb-6">
+                <h1 class="font-semibold text-3xl text-purple-900" style="font-family: 'Sniglet', cursive;">SaveSmart
+                </h1>
+                <p class="text-sm text-gray-700" style="font-family: 'Sniglet', cursive;">Connectez-vous à votre
+                    compte.</p>
+            </div>
 
             @if ($errors->any())
                 <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
@@ -40,26 +49,28 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="mb-4">
-                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email :</label>
+                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+                        style="font-family: 'Sniglet', cursive;">Email :</label>
                     <input type="email" id="email" name="email"
-                        class="shadow appearance-none border border-indigo-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        value="{{ old('email') }}" required>
+                        class="shadow appearance-none border border-purple-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value="{{ old('email') }}" required style="font-family: 'Sniglet', cursive;">
                 </div>
                 <div class="mb-6">
-                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Mot de passe :</label>
+                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2"
+                        style="font-family: 'Sniglet', cursive;">Mot de passe :</label>
                     <input type="password" id="password" name="password"
-                        class="shadow appearance-none border border-indigo-300 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        required>
+                        class="shadow appearance-none border border-purple-400 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        required style="font-family: 'Sniglet', cursive;">
                 </div>
                 <button type="submit"
-                    class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">Se
-                    connecter</button>
+                    class="bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                    style="font-family: 'Sniglet', cursive;">Se connecter</button>
             </form>
 
             <div class="mt-8 text-center">
-                <p>Pas encore inscrit ?
-                    <a href="{{ route('register') }}"
-                        class="text-indigo-500 hover:text-indigo-700 font-bold">Inscrivez-vous</a>
+                <p style="font-family: 'Sniglet', cursive;">Pas encore inscrit ?
+                    <a href="{{ route('register') }}" class="text-purple-500 hover:text-purple-700 font-bold"
+                        style="font-family: 'Sniglet', cursive;">Inscrivez-vous</a>
                 </p>
             </div>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,16 +5,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Sniglet&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Sniglet', cursive;
+            /* Apply Sniglet globally */
+        }
+    </style>
 </head>
 
-<body class="bg-gray-100">
-    <div class="container mx-auto py-10">
-        <div class="max-w-md mx-auto bg-white p-8 rounded shadow-md">
-            <h2 class="text-2xl font-semibold mb-6">Inscription</h2>
+<body class="h-screen overflow-hidden flex bg-indigo-50 text-gray-800">
+
+    <!-- Partie Image (50% de la largeur) -->
+    <div class="w-1/2 h-screen">
+        <img src="https://img.freepik.com/vecteurs-libre/illustration-concept-investissement_114360-3218.jpg?t=st=1740499341~exp=1740502941~hmac=87d6859c424de1ad670bd3574efad6bdd4119da5e5df4a765fb9902b84047e57&w=900"
+            alt="Illustration d'investissement ou de croissance financière" class="object-cover h-full w-full">
+    </div>
+
+    <!-- Partie Formulaire d'Inscription (50% de la largeur) -->
+    <div class="w-1/2 flex items-center justify-center">
+        <div class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 w-3/4">
+
+            <div class="text-center mb-6">
+                <h1 class="font-semibold text-3xl text-indigo-700" style="font-family: 'Sniglet', cursive;">SaveSmart
+                </h1>
+                <p class="text-sm" style="font-family: 'Sniglet', cursive;">Créez votre compte SaveSmart et commencez à
+                    gérer vos finances.</p>
+            </div>
 
             @if ($errors->any())
-                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
                     <ul>
                         @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
@@ -27,47 +52,50 @@
                 @csrf
 
                 <div class="mb-4">
-                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nom :</label>
+                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2"
+                        style="font-family: 'Sniglet', cursive;">Nom :</label>
                     <input type="text" id="name" name="name"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        value="{{ old('name') }}" required>
+                        class="shadow appearance-none border border-indigo-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value="{{ old('name') }}" required style="font-family: 'Sniglet', cursive;">
                 </div>
 
                 <div class="mb-4">
-                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email :</label>
+                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+                        style="font-family: 'Sniglet', cursive;">Email :</label>
                     <input type="email" id="email" name="email"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        value="{{ old('email') }}" required>
+                        class="shadow appearance-none border border-indigo-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        value="{{ old('email') }}" required style="font-family: 'Sniglet', cursive;">
                 </div>
 
                 <div class="mb-4">
-                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Mot de passe :</label>
+                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2"
+                        style="font-family: 'Sniglet', cursive;">Mot de passe :</label>
                     <input type="password" id="password" name="password"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        required>
+                        class="shadow appearance-none border border-indigo-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        required style="font-family: 'Sniglet', cursive;">
                 </div>
 
                 <div class="mb-6">
-                    <label for="password_confirmation" class="block text-gray-700 text-sm font-bold mb-2">Confirmation
+                    <label for="password_confirmation" class="block text-gray-700 text-sm font-bold mb-2"
+                        style="font-family: 'Sniglet', cursive;">Confirmation
                         du mot de passe :</label>
                     <input type="password" id="password_confirmation" name="password_confirmation"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        required>
+                        class="shadow appearance-none border border-indigo-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        required style="font-family: 'Sniglet', cursive;">
                 </div>
 
                 <div class="flex items-center justify-between">
                     <button type="submit"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        S'inscrire
-                    </button>
-                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                        href="{{ route('login') }}">
-                        Déjà un compte ? Se connecter
-                    </a>
+                        class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        style="font-family: 'Sniglet', cursive;">S'inscrire</button>
+                    <a class="inline-block align-baseline font-bold text-sm text-indigo-500 hover:text-indigo-700"
+                        href="{{ route('login') }}" style="font-family: 'Sniglet', cursive;">Déjà un compte ? Se
+                        connecter</a>
                 </div>
             </form>
         </div>
     </div>
+
 </body>
 
 </html>
