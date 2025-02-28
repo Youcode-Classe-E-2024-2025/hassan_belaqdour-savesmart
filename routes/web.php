e72cb1604d4c189a11720e6eed275507a9163527
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-02-28 15:58:04 +0000

    ajout de route pour category

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FamilyProfileController;
-use App\Http\Controllers\TransactionController; // Ajout de l'import pour TransactionController
-use Illuminate\Support\Facades\Auth; // Assure-toi que Auth est importé
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\CategoryController; // Ajout de l'import pour CategoryController
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,15 +19,14 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Modification de la route /home
 Route::get('/home', [FamilyProfileController::class, 'showDashboard'])->name('home')->middleware('auth');
 
-
-// Ajout des routes pour la sélection du profil
 Route::get('/select-profile', [FamilyProfileController::class, 'selectProfile'])->name('select_profile')->middleware('auth');
 Route::post('/select-profile', [FamilyProfileController::class, 'storeSelectedProfile'])->name('store_selected_profile')->middleware('auth');
 
 Route::resource('family_profiles', FamilyProfileController::class)->middleware('auth');
 
-// Ajout des routes pour les transactions
 Route::resource('transactions', TransactionController::class)->middleware('auth');
+
+// Ajout des routes pour les catégories
+Route::resource('categories', CategoryController::class)->middleware('auth');
