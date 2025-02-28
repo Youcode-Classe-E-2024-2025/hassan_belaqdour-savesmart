101cf3ce9fce2bca8ec52cdc257451b87370fb71
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-02-28 16:59:04 +0000

    Reapply "ajout du lien de register et login dans home"
    This reverts commit baf1574eed817cd78842796166e9218e3eeb8d93.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,7 @@
 <h1>hello</h1>
+<div class="space-x-4">
+    <a href="/register" class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
+        style="font-family: 'Sniglet', serif;">Register</a>
+    <a href="/login" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+        style="font-family: 'Sniglet', serif;">Login</a>
+</div>
