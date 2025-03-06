4acce1dd3e3b491ccf0d36492c0da4e610c73478
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-03-06 14:47:02 +0000

    ajout du table du revenue

---
+++ database/migrations/2025_03_06_144549_add_revenu_mensuel_to_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->decimal('revenu_mensuel', 10, 2)->nullable()->default(0); // Ajout du champ
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('revenu_mensuel'); // Suppression du champ
+        });
+    }
+};
